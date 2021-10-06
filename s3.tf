6f9420408376d05729e3ca432bf410cd4a067532
sivakolisetti <52354630+sivakolisetti@users.noreply.github.com>
2021-10-06 12:39:41 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "samplebucket" {
   bucket = var.bucketname
   acl    = "private"
   tags   = var.tags
