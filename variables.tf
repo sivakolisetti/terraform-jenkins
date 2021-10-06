1ed4ddd7f1e0a0ef00bdeee667369610c51b90a3
sivakolisetti <52354630+sivakolisetti@users.noreply.github.com>
2021-10-06 09:47:16 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,12 @@ variable "bucketname" {
   description = "Name of the s3 bucket"
   default     = "testmulti1"
 }
+
+variable "tags" {
+  type        = map
+  description = "All mandatory tags to use on all assets"
+  default = {
+    Env = "sample"
+    Code = "terraform in AWS"
+  }
+}
